4c332afa2b30a6d20db227d50e8a2c9d4560de79
danielporras7 <daniel.porras7@gmail.com>
2018-09-17 16:12:31 -0500

    agregue mas modulos

---
+++ modulo_uno/ejemploReporte/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Stock Move',
+    'version': '1.0',
+    'author': 'Luis Angel',
+    'website': 'http://www.soluciones4g.com',
+    'license': 'AGPL-3',
+    'summary': '',
+    'depends': [
+        'base','stock'
+        ],
+    'data': [
+        'report/report_move.xml',
+        'report/report_move_qweb.xml',
+        'views/stock_move_view_inherit.xml',
+        ],
+    'installable': True,
+}
